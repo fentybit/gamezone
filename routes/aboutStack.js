5c6a45c897a06e52d0e541f61308b7b7e6b13960
fentybit <fentymhall@gmail.com>
2021-06-28 11:28:06 -0700

    lesson 25 on Custom Header Component

--- routes/aboutStack.js
+++ routes/aboutStack.js
@@ -1,12 +1,16 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import About from '../screens/about';
 import ReviewDetails from '../screens/reviewDetails';
+import Header from '../shared/header';
+import React from 'react';
 
 const screens = {
     Home: {
         screen: About,
-        navigationOptions: {
-            title: 'About GameZone',
+        navigationOptions: ({ navigation }) => {
+            return {
+                headerTitle: () => <Header navigation={navigation} title="About GameZone" />,
+            }
         }
     },
 }
