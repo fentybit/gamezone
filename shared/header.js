34fbca4123a00dc2f34ed604ced5a4dd6c66b68a
fentybit <fentymhall@gmail.com>
2021-06-28 12:38:39 -0700

    lesson 28 on Background Images

--- shared/header.js
+++ shared/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Header({ title, navigation }) {
@@ -8,10 +8,13 @@ export default function Header({ title, navigation }) {
     }
 
     return (
-        <View style={styles.header}>
+        <ImageBackground source={require('../assets/game_bg.png')} style={styles.header}>
             <MaterialIcons name='menu' size={28} onPress={openMenu} style={styles.icon} />
-            <Text style={styles.headerText}>{title}</Text>
-        </View>
+            <View style={styles.headerTitle}>
+                <Image source={require('../assets/heart_logo.png')} style={styles.headerImage} />
+                <Text style={styles.headerText}>{title}</Text>
+            </View>
+        </ImageBackground>
     )
 }
 
@@ -19,7 +22,6 @@ const styles = StyleSheet.create({
     header: {
         width: '100%',
         height: '100%',
-        flex: 1,
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
@@ -32,8 +34,14 @@ const styles = StyleSheet.create({
     },
     icon: {
         position: 'absolute',
-        // alignItems: 'flex-start',
-
-        left: 16,
-    }
+        left: -40,
+    },
+    headerImage: {
+        width: 26,
+        height: 26,
+        marginHorizontal: 10
+    },
+    headerTitle: {
+        flexDirection: 'row'
+    },
 })
